a4f9de837e16720f5c3aa159e16d39e36cab29eb
Ramin Anvar <ramina1964@gmail.com>
2016-11-30 14:03:05 +0100

    Fixed NoOfPlayers and NoOfRounds for Development Purposes

--- ChessTournament/Program.cs
+++ ChessTournament/Program.cs
@@ -9,13 +9,16 @@ namespace ChessTournament
             try
             {
                 Console.WriteLine("Solution of Round Robin Setup\n");
-                Console.Write("Enter No. of Players:\t");
-                var noOfPlayers = Convert.ToInt32(Console.ReadLine());
+                //Console.Write("Enter No. of Players:\t");
+                //var noOfPlayers = Convert.ToInt32(Console.ReadLine());
 
-                Console.Write("Enter No. of Rounds:\t");
-                var noOfRounbds = Convert.ToInt32(Console.ReadLine());
+                //Console.Write("Enter No. of Rounds:\t");
+                //var noOfRounbds = Convert.ToInt32(Console.ReadLine());
 
-                var problemDesc = new ProblemDesc(noOfPlayers, noOfRounbds);
+                var noOfPlayers = 24;
+                var noOfRounds = 23;
+
+                var problemDesc = new ProblemDesc(noOfPlayers, noOfRounds);
                 var admin = new Admin();
 
                 Console.WriteLine(admin.ScreenSummary);
