6d43f4b3be980eadaddc8c7534aa339fa8868073
Ramin Anvar <ramina1964@gmail.com>
2016-11-30 13:50:58 +0100

    Added Exception Handling to the class Player.

--- ChessTournament/Player.cs
+++ ChessTournament/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ChessTournament
 {
@@ -6,17 +7,37 @@ namespace ChessTournament
 	{
 		public Player(int id, int rank)
 		{
-			// Add exception handling for id and rank.
-			// Id must be greater or equal than zero.
-			// Rank must be greater than zero.
-
 			Id = id;
 			Rank = rank;
 		}
 
-		internal int Id { get; }
-		internal int Rank { get; }
+		internal int Id
+		{
+			get { return _id; }
+
+			set
+			{
+				if(_id < 0 || _id > 1000000 )
+					throw new ArgumentOutOfRangeException(nameof(Id), value, "Value of Player ID is out of range!");
+			}
+		}
+
+		internal int Rank
+		{
+			get { return _rank; }
+
+			set
+			{
+				if (_rank < 0 || _rank > 3000)
+					throw new ArgumentOutOfRangeException(nameof(Rank), value, "Value of Rank is out of range!");
+			}
+		}
 
 		public int Compare(Player x, Player y) => x.Id > y.Id ? 1 : 0;
+
+
+		/*********************************************** Private Fields **********************************************/
+		private readonly int _id;
+		private readonly int _rank;
 	}
 }
