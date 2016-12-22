230a7e7de97696faf26fd4f25bf3a86728129cd9
Ramin Anvar <ramina1964@gmail.com>
2016-12-22 20:14:23 +0100

    In Utility 1) Changed signatire of        from    internal static Player FindPlayerById(IEnumerable<Player> players, int id)        to      internal static Player FindPlayerById(int id, IEnumerable<Player> players) 2) Removed methods FindGroup and FindPotentialPartnerFor

--- ChessTournament/Utility.cs
+++ ChessTournament/Utility.cs
@@ -12,25 +12,7 @@ namespace ChessTournament
 
 		internal const int StartPlayerId = 1;
 
-		internal static Player FindPlayerById(IEnumerable<Player> players, int id) => players.FirstOrDefault(p => p.Id == id);
-
-		internal static IEnumerable<Player> FindGroup(HashSet<HashSet<Match>> matches, List<Player> players)
-		{
-			var player = players.First();
-			var results = new List<Player> { player };
-			while (true)
-			{
-				var partner = FindPotentialPartnerFor(player.Id, matches, players);
-				if (partner != null && !results.Contains(partner))
-				{
-					results.Add(partner);
-					player = partner;
-				}
-
-				if (partner == null)
-					return results;
-			}
-		}
+		internal static Player FindPlayerById(int id, IEnumerable<Player> players) => players.FirstOrDefault(p => p.Id == id);
 
 		internal static List<Player> InitializePlayers
 		{
@@ -60,8 +42,8 @@ namespace ChessTournament
 			var innerList = new HashSet<Match>();
 			foreach (var item in variations)
 			{
-				var p1 = FindPlayerById(players, item[0]);
-				var p2 = FindPlayerById(players, item[1]);
+				var p1 = FindPlayerById(item[0], players);
+				var p2 = FindPlayerById(item[1], players);
 				var match = new Match(p1, p2);
 				if (p2.Id == StartPlayerId && innerList.Count == NoOfPlayers)
 				{
@@ -146,24 +128,14 @@ namespace ChessTournament
 			return sb.ToString();
 		}
 
-		private static bool AreListsEqual(IEnumerable<Player> fstPlayerList, IEnumerable<Player> sndPlayerList)
-		{ return fstPlayerList.All(fstItem => sndPlayerList.Any(sndItem => sndItem.Id == fstItem.Id)); }
-
 		internal static IEnumerable<Match> FindAllMatchesFor(Player player, IEnumerable<HashSet<Match>> allMatches, List<Player> players)
 		{ return allMatches.ElementAt(players.IndexOf(player)); }
 
 		/*********************************************** Private Fields **********************************************/
+		private static bool AreListsEqual(IEnumerable<Player> fstPlayerList, IEnumerable<Player> sndPlayerList)
+		{ return fstPlayerList.All(fstItem => sndPlayerList.Any(sndItem => sndItem.Id == fstItem.Id)); }
+
 		private static IList<int> GetAllPlayerIds(IEnumerable<Player> players) => players.Select(item => item.Id).ToList();
-
-		private static Player FindPotentialPartnerFor(int id, HashSet<HashSet<Match>> allMatches, List<Player> players)
-		{
-			var player = FindPlayerById(players, id);
-			var playerMatches = FindAllMatchesFor(player, allMatches, players);
-
-			return (from match in playerMatches
-					where match.FstPLayerId == id && !match.IsPlayed
-					select match.SndPlayer).FirstOrDefault();
-		}
 
 		private static int NoOfPlayers => ProblemDesc.NoOfPlayers;
 	}
