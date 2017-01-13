5a0817e75dc42947568cdcd56d08f3ee83f3085c
Ramin Anvar <ramina1964@gmail.com>
2017-01-13 14:12:02 +0100

    In Program.cs add a while loop to be able to read the input several times In PotentialPartners used functional Approach in the foreach loop

--- ChessTournament/Program.cs
+++ ChessTournament/Program.cs
@@ -2,35 +2,36 @@
 
 namespace ChessTournament
 {
-    internal class Program
-    {
-        private static void Main()
-        {
-            try
-            {
-                Console.WriteLine("Solution of Round Robin Setup\n");
-				Console.Write("Enter No. of Players:\t");
-				var noOfPlayers = Convert.ToInt32(Console.ReadLine());
-                var noOfRounds = noOfPlayers - 1;
+	internal class Program
+	{
+		private static void Main()
+		{
+			try
+			{
+				Console.WriteLine("Solution of Round Robin Setup\n");
+				while (true)
+				{
+					Console.Write("Enter No. of Players:\t");
+					var noOfPlayers = Convert.ToInt32(Console.ReadLine());
+					var noOfRounds = noOfPlayers - 1;
 
-                var problemDesc = new ProblemDesc(noOfPlayers, noOfRounds);
-                var admin = new Admin();
+					var problemDesc = new ProblemDesc(noOfPlayers, noOfRounds);
+					var admin = new Admin();
+					Console.WriteLine(admin.ScreenSummary);
+				}
+			}
 
-                Console.WriteLine(admin.ScreenSummary);
-                Console.ReadLine();
-            }
+			catch (ArgumentOutOfRangeException ex)
+			{
+				Console.WriteLine($"\n{ex.Message}");
+				Console.ReadLine();
+			}
 
-            catch (ArgumentOutOfRangeException ex)
-            {
-                Console.WriteLine($"\n{ex.Message}");
-                Console.ReadLine();
-            }
-
-            catch (Exception ex)
-            {
-                Console.WriteLine($"\n{ex.Message}");
-                Console.ReadLine();
-            }
-        }
-    }
+			catch (Exception ex)
+			{
+				Console.WriteLine($"\n{ex.Message}");
+				Console.ReadLine();
+			}
+		}
+	}
 }
